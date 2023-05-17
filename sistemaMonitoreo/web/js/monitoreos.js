86f743b29871b42d92e9a277b734400974e6398a
Hugo Rivera <hriveravz@gmail.com>
2023-05-17 00:20:25 -0700

    mapa con carrito

--- sistemaMonitoreo/web/js/monitoreos.js
+++ sistemaMonitoreo/web/js/monitoreos.js
@@ -67,3 +67,71 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), mapOptions); // Create the map
     window.initMap = initMap;
 }
+function initMap() {
+    var mapCenter = { lat: 27.741145211270663, lng: -109.30536124442628 }; // Set the coordinates of the map center
+    var mapOptions = { zoom: 12, center: mapCenter, zoomControl: false, streetViewControl:false,fullscreenControl:false
+    ,streetViewControl: false,mapTypeControl: false,mapTypeId: 'satellite'}; // Set the initial zoom level and center of the map
+    var directionsService = new google.maps.DirectionsService();
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    var locations = [
+        '27.739301868948875, -109.30808807563652',
+        '27.736073, -109.310073',
+        '27.747743, -109.305412'
+      ]; // Create the map
+      var colors = [
+        'RED',
+        'blue',
+        'green'
+      ];
+      var waypointsArray = [
+        { location: '27.739301868948875, -109.30808807563652' },
+        { location: '27.736073, -109.310073' },
+        { location: '27.747743, -109.305412' }
+      ];
+     // for (var i = 0; i < locations.length; i++) {
+    var request = {
+        origin: '27.74095868652802, -109.3053437359123',
+        destination: '27.737101, -109.309195',
+        travelMode: 'DRIVING', // or 'WALKING', 'TRANSIT', 'BICYCLING'
+        waypoints: waypointsArray,
+        optimizeWaypoints: false // Optional: Optimize the order of waypoints for the best route
+      };
+      
+      directionsService.route(request, function(response, status) {
+        if (status === 'OK') {
+          var directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers:true});
+          directionsRenderer.setDirections(response);
+          directionsRenderer.setMap(map);
+          animateCar(response,map);
+        }
+        // directionsRenderer.setOptions({
+        //     polylineOptions: {
+        //       strokeColor: colors[i] // Set different colors for each route
+        //     }
+        //   });
+      });
+    //}
+    window.initMap = initMap;
+  }
+
+  function animateCar(route,map) {
+    var step = 0; // Initialize the step counter
+    var numSteps = route.routes[0].overview_path.length; // Get the total number of steps in the route
+    var icon = {
+        url: 'images/car.png', // Replace with the path to your icon image
+        scaledSize: new google.maps.Size(35, 35) // Set the desired width and height
+      };
+    var carMarker = new google.maps.Marker({
+      position: route.routes[0].overview_path[0],
+      map: map,
+      icon: icon // Replace with the path to your car icon image
+    });
+  
+    setInterval(function() {
+      step = (step + 1) % numSteps; // Increment the step counter and wrap around
+  
+      var position = route.routes[0].overview_path[step]; // Get the current position along the route
+      carMarker.setPosition(position); // Update the car marker position on the map
+    }, 1000); // Adjust the interval (in milliseconds) to control the speed of the animation
+  }
