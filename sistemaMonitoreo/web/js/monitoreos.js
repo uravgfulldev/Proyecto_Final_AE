ff183b09f05b9fc3ee8a37fe1b9cc7e52866ff92
Hugo Rivera <hriveravz@gmail.com>
2023-05-17 02:08:12 -0700

    mapa terminado

--- sistemaMonitoreo/web/js/monitoreos.js
+++ sistemaMonitoreo/web/js/monitoreos.js
@@ -48,76 +48,107 @@ function createJWT(mensaje) {
 }
 
 function initMap() {
-    var mapCenter = { lat: 27.741145211270663, lng: -109.30536124442628 }; // Set the coordinates of the map center
-    var mapOptions = { zoom: 12, center: mapCenter }; // Set the initial zoom level and center of the map
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions); // Create the map
-    window.initMap = initMap;
-}
-function initMap() {
-    var mapCenter = { lat: 27.741145211270663, lng: -109.30536124442628 }; // Set the coordinates of the map center
-    var mapOptions = { zoom: 12, center: mapCenter, zoomControl: false, streetViewControl:false,fullscreenControl:false
-    ,streetViewControl: false,mapTypeControl: false,mapTypeId: 'satellite'}; // Set the initial zoom level and center of the map
-    var directionsService = new google.maps.DirectionsService();
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    
-    var locations = [
-        '27.739301868948875, -109.30808807563652',
-        '27.736073, -109.310073',
-        '27.747743, -109.305412'
-      ]; // Create the map
-      var colors = [
-        'RED',
-        'blue',
-        'green'
-      ];
-      var waypointsArray = [
-        { location: '27.739301868948875, -109.30808807563652' },
-        { location: '27.736073, -109.310073' },
-        { location: '27.747743, -109.305412' }
-      ];
-     // for (var i = 0; i < locations.length; i++) {
-    var request = {
-        origin: '27.74095868652802, -109.3053437359123',
-        destination: '27.737101, -109.309195',
-        travelMode: 'DRIVING', // or 'WALKING', 'TRANSIT', 'BICYCLING'
-        waypoints: waypointsArray,
-        optimizeWaypoints: false // Optional: Optimize the order of waypoints for the best route
-      };
-      
-      directionsService.route(request, function(response, status) {
-        if (status === 'OK') {
-          var directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers:true});
-          directionsRenderer.setDirections(response);
-          directionsRenderer.setMap(map);
-          animateCar(response,map);
-        }
-        // directionsRenderer.setOptions({
-        //     polylineOptions: {
-        //       strokeColor: colors[i] // Set different colors for each route
-        //     }
-        //   });
-      });
-    //}
-    window.initMap = initMap;
-  }
+  var mapCenter = { lat:  31.390347, lng: -110.605142}; // Set the coordinates of the map center
+  var mapOptions = { zoom: 10, center: mapCenter ,zoomControl: false, fullscreenControl: false,mapTypeControl:false,
+  streetViewControl:false}; // Set the initial zoom level and center of the map
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions); // Create the map
+  
+//Red road
+var waypointsArray = [
+  { location: '31.403964, -110.590369' },
+  { location: '31.394831, -110.591702' },
+  { location: '31.390933, -110.603802' }];
+  var directionsService = new google.maps.DirectionsService();
 
-  function animateCar(route,map) {
-    var step = 0; // Initialize the step counter
-    var numSteps = route.routes[0].overview_path.length; // Get the total number of steps in the route
-    var icon = {
-        url: 'images/car.png', // Replace with the path to your icon image
-        scaledSize: new google.maps.Size(35, 35) // Set the desired width and height
-      };
-    var carMarker = new google.maps.Marker({
-      position: route.routes[0].overview_path[0],
-      map: map,
-      icon: icon // Replace with the path to your car icon image
-    });
-  
-    setInterval(function() {
-      step = (step + 1) % numSteps; // Increment the step counter and wrap around
-  
-      var position = route.routes[0].overview_path[step]; // Get the current position along the route
-      carMarker.setPosition(position); // Update the car marker position on the map
-    }, 1000); // Adjust the interval (in milliseconds) to control the speed of the animation
+  var request = {
+  origin: '31.405073, -110.611535',
+  destination: '31.405148, -110.610998',
+  travelMode: 'DRIVING', // or 'WALKING', 'TRANSIT', 'BICYCLING'
+  waypoints: waypointsArray,
+  optimizeWaypoints: true // Optional: Optimize the order of waypoints for the best route
+};
+
+directionsService.route(request, function(response, status) {
+  if (status === 'OK') {
+    var directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers:true});
+    directionsRenderer.setDirections(response);
+    directionsRenderer.setMap(map);
+    animateCar(response,map);
   }
+   directionsRenderer.setOptions({
+      polylineOptions: {
+         strokeColor: "red" // Set different colors for each route
+       }
+     });
+});
+
+//Blue road
+  var directionsService = new google.maps.DirectionsService();
+
+  var request = {
+  origin: '31.403389, -110.590272',
+  destination: '31.377398, -110.569969',
+  travelMode: 'DRIVING', // or 'WALKING', 'TRANSIT', 'BICYCLING'
+  optimizeWaypoints: true // Optional: Optimize the order of waypoints for the best route
+};
+
+directionsService.route(request, function(response, status) {
+  if (status === 'OK') {
+    var directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers:true});
+    directionsRenderer.setDirections(response);
+    directionsRenderer.setMap(map);
+    animateCar(response,map);
+  }
+   directionsRenderer.setOptions({
+      polylineOptions: {
+         strokeColor: "blue" // Set different colors for each route
+       }
+     });
+});
+//Green road
+var directionsService = new google.maps.DirectionsService();
+
+var request = {  
+origin: '31.390953, -110.603824',
+destination: '31.385413, -110.618101',
+travelMode: 'DRIVING', // or 'WALKING', 'TRANSIT', 'BICYCLING'
+optimizeWaypoints: true // Optional: Optimize the order of waypoints for the best route
+};
+
+directionsService.route(request, function(response, status) {
+if (status === 'OK') {
+  var directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers:true});
+  directionsRenderer.setDirections(response);
+  directionsRenderer.setMap(map);
+  animateCar(response,map);
+}
+ directionsRenderer.setOptions({
+    polylineOptions: {
+       strokeColor: "green" // Set different colors for each route
+     }
+   });
+});
+window.initMap = initMap;
+}
+
+
+function animateCar(route,map) {
+var step = 0; // Initialize the step counter
+var numSteps = route.routes[0].overview_path.length; // Get the total number of steps in the route
+var icon = {
+  url: 'images/car.png', // Replace with the path to your icon image
+  scaledSize: new google.maps.Size(35, 35) // Set the desired width and height
+};
+var carMarker = new google.maps.Marker({
+position: route.routes[0].overview_path[0],
+map: map,
+icon: icon // Replace with the path to your car icon image
+});
+
+setInterval(function() {
+step = (step + 1) % numSteps; // Increment the step counter and wrap around
+
+var position = route.routes[0].overview_path[step]; // Get the current position along the route
+carMarker.setPosition(position); // Update the car marker position on the map
+}, 1000); // Adjust the interval (in milliseconds) to control the speed of the animation
+}
